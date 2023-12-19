79097fcbe7b15978b1b8df4baa8395335ad81000
Jesse Forrest <forrestj04@gmail.com>
2023-12-19 11:41:36 -0800

    Upds

--- lua/circadian-colors.lua
+++ lua/circadian-colors.lua
@@ -1,25 +1,45 @@
 local M = {}
 
-local start_hour = vim.g.circadian_start_hour or 8
-local end_hour = vim.g.circadian_end_hour or 20
+M.start_hour = 8
+M.end_hour = 20
 
 function M.set_colorscheme()
     local hour = os.date("*t").hour
-    if hour >= start_hour and hour < end_hour then
+    if hour >= M.start_hour and hour < M.end_hour then
         vim.cmd("colorscheme rose-pine-dawn")
     else
         vim.cmd("colorscheme rose-pine")
     end
 end
 
-vim.api.nvim_create_autocmd("VimEnter", {callback = M.set_colorscheme})
+function M.set_hours(start_hour, end_hour)
+    M.start_hour = start_hour
+    M.end_hour = end_hour
+    M.set_colorscheme() -- Update the colorscheme immediately
+end
+
+vim.api.nvim_create_autocmd("VimEnter", { callback = M.set_colorscheme })
 
 vim.api.nvim_create_autocmd("VimEnter", {
     callback = function()
         M.set_colorscheme()
-        vim.api.nvim_create_timer(60000, {callback = M.set_colorscheme}) -- checks every 60 seconds
+        vim.api.nvim_create_timer(60000, { callback = M.set_colorscheme }) -- checks every 60 seconds
     end
 })
 
-return M
+vim.api.nvim_create_user_command('CircadianSetHours', function(opts)
+    local args = vim.split(opts.args, " ")
+    if #args == 2 then
+        local start_hour = tonumber(args[1])
+        local end_hour = tonumber(args[2])
+        if start_hour and end_hour then
+            M.set_hours(start_hour, end_hour)
+        else
+            print("Invalid hours. Please enter numeric values.")
+        end
+    else
+        print("Please enter two numbers: start hour and end hour.")
+    end
+end, { nargs = "*" })
 
+return M
