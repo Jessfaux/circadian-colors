1302bb33f1d11e8c68183d775e77407dffd7d4d6
Jesse Forrest <forrestj04@gmail.com>
2023-12-19 11:37:54 -0800

    Added time of day config and auto checker

--- lua/circadian-colors.lua
+++ lua/circadian-colors.lua
@@ -1,12 +1,25 @@
 local M = {}
 
+local start_hour = vim.g.circadian_start_hour or 8
+local end_hour = vim.g.circadian_end_hour or 20
+
 function M.set_colorscheme()
     local hour = os.date("*t").hour
-    if hour >= 8 and hour < 20 then
+    if hour >= start_hour and hour < end_hour then
         vim.cmd("colorscheme rose-pine-dawn")
     else
         vim.cmd("colorscheme rose-pine")
     end
 end
 
+vim.api.nvim_create_autocmd("VimEnter", {callback = M.set_colorscheme})
+
+vim.api.nvim_create_autocmd("VimEnter", {
+    callback = function()
+        M.set_colorscheme()
+        vim.api.nvim_create_timer(60000, {callback = M.set_colorscheme}) -- checks every 60 seconds
+    end
+})
+
 return M
+
